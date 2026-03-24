1a21443fcd52e5047a97976bc7858d1b5435b532
yaros255 <yarbon@alu.edu.gva.es>
2026-03-24 16:08:14 +0100

    Añadido print registro

--- GestorDeTareasWinForms/Program.cs
+++ GestorDeTareasWinForms/Program.cs
@@ -12,6 +12,7 @@ namespace GestorDeTareasWinForms
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            Console.WriteLine("Registro implementado");
         }
     }
 }
